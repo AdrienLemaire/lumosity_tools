a2f2d122b57a2ba36807e061a7934492bbe83c36
Adrien Lemaire <dori@alpacadb.com>
2015-06-30 11:59:34 +0900

    update readme, add reqs, script is now working

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # lumosity_tools
 Tools to get more efficient when training at Lumosity
+
+
+# Installation
+
+Build your virtual environment with python 3:
+
+    $ mkvirtualenv -p /usr/bin/python3.4 lumosity
+
+Install the dependencies:
+
+    $ pip install -r requirements.txt
+
+
+# Usage
+
+Get today games LPI:
+
+    $ ./fetch_lpi.py
--- fetch_lpi.py
+++ fetch_lpi.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import argparse
+from bs4 import BeautifulSoup as bs
 import datetime
 import json
 import os.path
@@ -10,23 +11,37 @@ import requests
 from local_settings import EMAIL, PASSWORD
 
 
-def main(date=datetime.datetime.today().strftime("%Y-%M-%d")):
+def main(date=None):
+    if not date:
+        date = datetime.datetime.today().strftime("%Y-%m-%d")
+    data = {}
     with requests.Session() as s:
         # 1. login
         url = 'https://www.lumosity.com/login'
         s.get(url, verify=True)  # get cookies
         payload = {
-            'email': EMAIL,
-            'password': PASSWORD,
+            'user[login]': EMAIL,
+            'user[password]': PASSWORD,
             #'csrfmiddlewaretoken': s.cookies['csrftoken'],
         }
         s.post(url, data=payload, headers={'Referer': url})
-        import ipdb; ipdb.set_trace()
 
         # 2. Get 
         url = 'http://www.lumosity.com/app/v4/training_history/summaries/{}'
+        print(url.format(date))
         r = s.get(url.format(date))
-        print r.url
+        content = r.content
+        p = bs(r.content)
+        raw= str([s for s in p.find_all('script') if "Summaries.Show.init" in
+            s.text][0])
+        data = json.loads(raw[raw.find('{', raw.find('{')+1):raw.rfind('}', 0,
+            raw.rfind('}'))+1])
+
+    results = data['calendarData'][0]['data']['results']
+    for area in results.keys():
+        print(area)
+        for game in results[area]:
+            print('\t* {}:{}'.format(game['game'], game['bpi']))
 
 
 def parse_args():
